accf97f2c631f2a1b6d899c3645effa0f8f50128
Yingge Sun <yingges@user001.palmetto.clemson.edu>
2017-05-03 18:49:32 -0400

    Added new plotting script

--- scripts/plot_log.py
+++ scripts/plot_log.py
@@ -1,12 +1,15 @@
-import sys
+import sys, os
 import matplotlib.pyplot as plt
+import subprocess
 
 if __name__ == '__main__':
     
-    if len(sys.argv) < 1:
-        print('Please provide converted input log data.')
-    else:
-        infile = open(sys.argv[1], 'r')
+    if len(sys.argv) < 3:
+        print('Usage:\n \t./plot_log.py <log_file> <ylim.max>')
+        sys.exit()
+
+    subprocess.call(['./parse_log.sh', sys.argv[1]])
+    infile = open('aux.txt', 'r')
 
     iterations = []
     loss = []
@@ -16,7 +19,7 @@ if __name__ == '__main__':
         loss.append(float(linestr[0]))
         iterations.append(int(linestr[1]))
 
-    plt.ylim(ymax=3, ymin=0)
+    plt.ylim(ymax=float(sys.argv[2]), ymin=0)
     plt.grid(True)
 
     plt.xlabel('Iterations')
@@ -26,3 +29,6 @@ if __name__ == '__main__':
     # plt.show('yolo_loss.png')
     
     plt.savefig('yolo_loss.png')
+
+    if open('aux.txt', 'r'):
+        os.remove('aux.txt')
